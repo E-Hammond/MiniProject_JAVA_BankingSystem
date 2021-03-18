013c5b543365c67601a260b05a81c082ed1c0404
E-Hammond <emmanuel.hammond@amalitech.org>
2021-03-18 09:54:21 +0000

    Implemented ICustomer.java file

---
+++ src/main/java/org/interfaces/ICustomer.java
@@ -0,0 +1,9 @@
+package org.interfaces;
+import java.util.ArrayList;
+
+
+public interface ICustomer {
+    public String getName();
+    public ArrayList<Double> getTransactions();
+    public void addTransaction(double transaction);
+}
